d213c5fa10589a2188a28bff7dbdca61b9cbc17f
JovanVian <168513405+JovanVian13@users.noreply.github.com>
2024-06-03 15:12:15 +0700

    Update statistic.jsx

--- src/components/statistic.jsx
+++ src/components/statistic.jsx
@@ -18,7 +18,7 @@ const Statistics = () => {
             <div className="items-center text-container justify-center flex flex-col lg:flex-row min-h-screen gap-12 px-10">
                 <img src={StatsHouse} className="rounded-xl w-11/12 lg:w-1/2"/>
                 <div className="w-full">
-                    <div className="items-center text-container justify-center flex w-11/12 lg:w-1/2">
+                    <div className="items-center text-container justify-center w-11/12 lg:w-1/2">
                         <h1 className="text-3xl font-extrabold text-defaultColor">We Help You To Find Your Dream Home</h1>
                         <br />
                         <p className="text-sm text-textColor font-semibold text-justify">From cozy cottages to luxurious estates, our dedicated team guides you through every step of the journey, ensuring your dream home becomes a reality</p>
